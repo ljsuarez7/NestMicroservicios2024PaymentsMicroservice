a64d79a9ac3ba2460df8c3923a0b8ab2c0a98cf6
JaviSrz <ljsuarez7@gmail.com>
2024-09-17 18:29:33 +0200

    configuración basica de stripe y payment session

--- src/configs/envs.ts
+++ src/configs/envs.ts
@@ -3,10 +3,12 @@ import * as joi from 'joi';
 
 interface EnvVars{
     PORT: number;
+    STRIPE_SECRET: string;
 }
 
 const envsSchema = joi.object({
-    PORT: joi.number().required()
+    PORT: joi.number().required(),
+    STRIPE_SECRET: joi.string().required()
 })
 .unknown(true);
 
@@ -19,7 +21,8 @@ if(error){
 const envVars: EnvVars = value;
 
 export const envs = {
-    port: envVars.PORT
+    port: envVars.PORT,
+    stripeSecret: envVars.STRIPE_SECRET
 }
 
 
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { envs } from 'src/configs';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+
+    private readonly stripe = new Stripe(envs.stripeSecret);
+
+    async createPaymentSession(paymentSessionDto: PaymentSessionDto){
+
+        const {currency, items} = paymentSessionDto;
+
+        const lineItems = items.map(item => {
+            return {
+                price_data: {
+                    currency: currency,
+                    product_data: {
+                        name: item.name
+                    },
+                    unit_amount: Math.round(item.price * 100), //20 dolares porque no se pueden mandar 20.00
+                },
+                quantity: item.quantity
+            }
+        })
+
+        const session = await this.stripe.checkout.sessions.create({
+            //Colocar aqui el ID de la orden
+            payment_intent_data: {
+                metadata: {}
+            },
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: 'http://localhost:3003/payments/success',
+            cancel_url: 'http://localhost:3003/payments/cancel',
+        });
+
+        return session;
+
+    }
+
+}
